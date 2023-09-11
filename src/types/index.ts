cfe70c8ffa762c2df3976a89b0ffea05af706dda
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-09-10 23:33:18 -0500

    chore: add types

---
+++ src/types/index.ts
@@ -0,0 +1,44 @@
+export type Company = {
+  name: string;
+  phone: number[];
+  email: string[];
+  social: Social;
+  location: string[];
+  schedule: Schedule;
+  Characteristic: string[];
+};
+
+export type Social = {
+  facebook: string;
+  instagram: string;
+  twitter: string;
+};
+
+export type Schedule = {
+  "Lunes - Viernes": string[];
+  Sabado: string[];
+};
+
+export type Benefit = {
+  icon: string;
+  description: string;
+};
+
+export type Plan = {
+  price: string;
+  plan: string;
+  benefits: string[];
+};
+
+export type requirement = {
+  icon: string;
+  title: string;
+  description: string;
+};
+
+export type Testimonial = {
+  image: string;
+  name: string;
+  location: string;
+  description: string;
+};
