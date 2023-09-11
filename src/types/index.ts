fcaf44f4df43a5d729eaed90d9fec80c8163ebf4
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-09-11 04:27:49 -0500

    chore: update path assets

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,50 +1,44 @@
-export interface Page{
-  label: string; 
+export interface Page {
+  label: string
   href: string
 }
 
 export interface Company {
-  name: string;
-  phone: number[];
-  email: string[];
-  social: Social;
-  location: string[];
-  schedule: Schedule;
-  Characteristic: string[];
+  name: string
+  phone: number[]
+  email: string[]
+  social: Social
+  location: string[]
+  schedule: Schedule
+  Characteristic: string[]
 }
 
 export interface Social {
-  facebook: string;
-  instagram: string;
-  twitter: string;
+  facebook: string
+  instagram: string
+  twitter: string
 }
 
 export interface Schedule {
-  weekDays: string[];
-  weekEnds: string[];
+  weekDays: string[]
+  weekEnds: string[]
 }
 
 export interface Benefit {
-  icon: string;
-  description: string;
+  icon: string
+  description: string
+  title?: string
 }
 
 export interface Plan {
-  price: string;
-  plan: string;
-  benefits: string[];
-}
-
-export interface Requirement {
-  icon: string;
-  title: string;
-  description: string;
+  price: string
+  plan: string
+  benefits: string[]
 }
 
 export interface Testimonial {
-  image: string;
-  name: string;
-  location: string;
-  description: string;
+  image: string
+  name: string
+  location: string
+  description: string
 }
-
