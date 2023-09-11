e47dc8c8ce34b104f91894f540f5f8d55cb7e997
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-09-10 23:33:56 -0500

    chore: add data

---
+++ src/data/plans.ts
@@ -0,0 +1,48 @@
+import { Plan } from "@/types";
+
+export const INTERNET_PLANS: Plan[] = [
+  {
+    price: "$44.000",
+    plan: "20MB",
+    benefits: [
+      "Navega",
+      "Descarga",
+      "Transmite",
+      "Juega en vivo ",
+      "Reproduce",
+      "videos",
+    ],
+  },
+  {
+    price: "$110.000",
+    plan: "50MB",
+    benefits: [
+      "Navega",
+      "Descarga",
+      "Transmite",
+      "Juega en vivo ",
+      "Reproduce",
+      "videos",
+    ],
+  },
+  {
+    price: "$88.000",
+    plan: "40MB",
+    benefits: [
+      "Navega",
+      "Descarga",
+      "Transmite",
+      "Juega en vivo ",
+      "Reproduce",
+      "videos",
+    ],
+  },
+];
+
+export const TELEVISION_PLANS: Plan[] = [
+  {
+    price: "$40.000",
+    plan: "+110 canales digitales",
+    benefits: ["Dospuntos de televisión", "+ canales exclusivos"],
+  },
+];
