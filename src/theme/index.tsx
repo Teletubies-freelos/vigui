c7536bdad0640604e2b193c98e764f2ff26d6822
luiggy <luiggy.carlin@brivesoluciones.com>
2023-09-12 02:49:51 -0500

    style: add about us styles

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,33 +1,33 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
 
 export const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: "#FF4C00",
+      main: '#FF4C00',
     },
     secondary: {
-      main: "#0039A4",
-      "900": "#0E58EA",
-      contrastText: "#FF912C",
+      main: '#0039A4',
+      '900': '#0E58EA',
+      contrastText: '#FF912C',
     },
     background: {
-      default: "#0039A4",
+      default: '#0039A4',
     },
     info: {
-      main: "#0E58EA",
+      main: '#0E58EA',
     },
   },
   typography: {
     body1: {
-      color: "white",
+      color: 'white',
     },
-    body2:{
-      color: '#252525'
+    body2: {
+      color: '#252525',
     },
-    h3:{
+    h3: {
       color: '#0E58EA',
       fontWeight: 'bold',
-      fontSize: '2em'
-    }
-  }
-})
+      fontSize: '2em',
+    },
+  },
+});
