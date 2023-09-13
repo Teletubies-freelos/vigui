8687f590a9cd965b85aeeb01a26ff3d6485a43a1
luiggy <luiggy.carlin@brivesoluciones.com>
2023-09-12 20:12:21 -0500

    carousel render on page

--- src/stories/Organisms/Carousel/CustomCarousel.example.tsx
+++ src/stories/Organisms/Carousel/CustomCarousel.example.tsx
@@ -1,47 +1,47 @@
-import { Card } from "@/components/card";
-import ResponsiveCarousel from "@/components/carousel";
-import { ThemeRegistry } from "@/providers/theme";
-import { Box } from "@mui/material";
-import ContentCard from "./ContentCard";
-import HeaderCard from "./HeaderCard";
+import { Card } from '@/components/card';
+import ResponsiveCarousel from '@/components/carousel';
+import { ThemeRegistry } from '@/providers/theme';
+import { Box } from '@mui/material';
+import ContentCard from './ContentCard';
+import HeaderCard from './HeaderCard';
 
 export default function CustomCarousel() {
   const data = [
     {
-      price: "$ 100",
-      velocity: "20",
-      moreTitle: "De 20Mb a 500Mb",
+      price: '$ 100',
+      velocity: '20',
+      moreTitle: 'De 20Mb a 500Mb',
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?",
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?',
     },
     {
-      price: "$ 200",
-      velocity: "40",
-      moreTitle: "De 20Mb a 500Mb",
+      price: '$ 200',
+      velocity: '40',
+      moreTitle: 'De 20Mb a 500Mb',
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?",
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?',
     },
     {
-      price: "$ 300",
-      velocity: "60",
-      moreTitle: "De 20Mb a 500Mb",
+      price: '$ 300',
+      velocity: '60',
+      moreTitle: 'De 20Mb a 500Mb',
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?",
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?',
     },
     {
-      price: "$ 500",
-      velocity: "60",
-      moreTitle: "De 20Mb a 500Mb",
+      price: '$ 500',
+      velocity: '60',
+      moreTitle: 'De 20Mb a 500Mb',
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?",
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat aspernatur odio error provident saepe id asperiores eius doloremque labore deleniti?',
     },
   ];
 
   const itemRender = (item: any) => (
     <Card
       sxCard={{
-        width: "16rem",
-        margin: "0 auto",
+        width: '16rem',
+        margin: '0 auto',
       }}
       header={<HeaderCard item={item} />}
       content={<ContentCard item={item} />}
@@ -50,7 +50,7 @@ export default function CustomCarousel() {
 
   return (
     <ThemeRegistry>
-      <Box width="100%">
+      <Box width='100%'>
         <ResponsiveCarousel data={data} itemRender={itemRender} groupSize={3} />
       </Box>
     </ThemeRegistry>
